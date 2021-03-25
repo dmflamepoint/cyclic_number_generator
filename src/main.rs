487fae156b2510b514ace676f6ac9bea68399769
David Miller <dm.flamepoint@gmail.com>
2021-03-24 22:14:08 -0500

    Cleaned up comments

--- src/main.rs
+++ src/main.rs
@@ -4,45 +4,39 @@
 // This program will find large cyclic numbers by taking advantage of the relationship between
 // cyclic numbers and primitve roots module p. To do so, there needs to be a relatively fast
 // algorithm for finding the prime factorization of a number x.
-//
-// The current idea is to use memoization to create a function that generates a list of prime
-// numbers up to and including some number x (or sqrt(x) for a speed improvement).
-//
-// The problem is where to store the saved primes after the function call to "generate_primes"
-// returns. It would be nice to have a global vector that could be allocated on the heap and added
-// to as needed.
 
 
-
+// Primes is a structure that stores the current list of primes needed for the euler_phi function
 pub struct Primes(Vec<u32>);
 
 impl Primes {
     pub fn add_primes(&mut self, x: u32) -> &Vec<u32> {
-        println!("Array before adding to {} is \n{:?}\n", x, self.0);
+        //println!("Array before adding to {} is \n{:?}\n", x, self.0);
 
         let length: usize = self.0.len();
-        let mut largest_prime: u32 = 0;
+        let mut largest_prime_candidate: u32 = 0;
         if length >= 2 {
-            largest_prime = self.0[length-1];
+            largest_prime_candidate = self.0[length-1];
         } else {
+            // push 2 and 3 so the largest prime is odd
             self.0.push(2u32);
             self.0.push(3u32);
-            largest_prime = 3; 
+            largest_prime_candidate = 3; 
         }
-        // go from largest_prime+2 to x (counting by 2's) and add prime numbers found
         
+        // counting by 2's and adding prime numbers found until x is reached
         let mut is_prime: bool = true;
-        while largest_prime < x {
-            largest_prime += 2;
+        while largest_prime_candidate < x {
+            largest_prime_candidate += 2;
             for prime in &self.0 {
-                if largest_prime % prime == 0 {
+                if largest_prime_candidate % prime == 0 {
                     is_prime = false;
                     break;
                 }
             }
             
             if is_prime {
-                self.0.push(largest_prime);
+                self.0.push(largest_prime_candidate);
             } else {
                 is_prime = true;
             }
@@ -91,8 +85,6 @@ fn prime_factorization(list_of_primes: &mut Primes, x: u32) -> Vec<(u32,u32)> {
             factors.push((prime, power));
         }
     }
-    
-    
     return factors;
 }
 
@@ -104,16 +96,6 @@ fn main() {
     let _a = list_of_primes.add_primes(1000);
     let _b = list_of_primes.add_primes(10000);
     let _c = list_of_primes.add_primes(100000);
-
-    
-
-
-
-
-
-
-
-
 
     //let mut test_value: u32 = 50;
     //let mut result = euler_phi(&mut list_of_primes, test_value);
